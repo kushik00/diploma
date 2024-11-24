e55f0ca2cf27556aee247d73f31c24585f4c32ce
kushik00 <146574580+kushik00@users.noreply.github.com>
2024-11-24 16:07:30 +0200

    Add files via upload

--- lada.3-semestr.diploma/script.js
+++ lada.3-semestr.diploma/script.js
@@ -1,45 +1,5 @@
 
 
-let offset = 0;
-const sliderLine = document.querySelector('.serfiticat');
-
-document.querySelector('.slider-next').addEventListener('click', function(){
-    offset = offset - 600;
-    if (offset > 1800){
-        offset = 0;
-    }
-    sliderLine.style.left = offset + 'px'
-})
-
-document.querySelector('.slider-prev').addEventListener('click', function(){
-    offset = offset + 600;
-    if (offset < 1800){
-        offset = 0;
-    }
-    sliderLine.style.left = offset + 'px'
-})
-
-
-
-const menuContainer = document.querySelector('.menu-container');
-const menuBurger = document.querySelector('.menu-burger');
-const closeBtn = document.querySelector('.close-btn');
-
-// Открыть меню
-menuBurger.addEventListener('click', function() {
-    menuContainer.classList.toggle('closed');
-});
-
-// Закрыть меню
-closeBtn.addEventListener('click', function() {
-    menuContainer.classList.add('closed');
-});
-
-
-
-
-
-
 var arrLang = { 
     'ru': {
         'first': 'главная',
@@ -161,4 +121,44 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
+});
+
+
+const menuContainer = document.querySelector('.menu-container');
+const menuBurger = document.querySelector('.menu-burger');
+const closeBtn = document.querySelector('.close-btn');
+
+// Открыть меню
+menuBurger.addEventListener('click', function() {
+    menuContainer.classList.toggle('closed');
+});
+
+// Закрыть меню
+closeBtn.addEventListener('click', function() {
+    menuContainer.classList.add('closed');
+});
+
+
+
+
+
+
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 256;
+    if (offset > 768) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset = offset - 256;
+    if (offset < 0) {
+        offset = 768;
+    }
+    sliderLine.style.left = -offset + 'px';
 });
